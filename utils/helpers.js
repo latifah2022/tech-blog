5f029fecad47daec3cfe6d0d09be9a8a353e4099
Latifah Zawedde <zawedde2021@gmail.com>
2022-10-12 18:41:38 -0700

    updating blog handlebars

--- utils/helpers.js
+++ utils/helpers.js
@@ -19,4 +19,16 @@ module.exports = {
     //     return `<span for="img" aria-label="gear">⚙️</span>`;
     //   }
     //},
-  };
+};
+
+module.exports = {
+  format_time: (date) => {
+    return date.toLocaleTimeString();
+  },
+  format_date: (date) => {
+    return `${new Date(date).getMonth() + 1}/${new Date(date).getDate()}/${
+      new Date(date).getFullYear() + 5
+    }`;
+  },
+};
+
